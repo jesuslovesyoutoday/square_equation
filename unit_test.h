7f5de2bcc6f07c1ac5adbaef6d3d9d89ffd9de9a
jesuslovesyoutoday <73426135+jesuslovesyoutoday@users.noreply.github.com>
2021-09-09 00:36:02 +0300

    correct header

---
+++ unit_test.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+//------------------------------------------------------
+//! Tester for Square Equation Solver
+//!
+//! @note: checks a few main cases of possible equations
+//------------------------------------------------------
+
+void Unit_Test();
